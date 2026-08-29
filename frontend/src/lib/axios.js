43c95a7ce654942e90aa1d11e24c0574e32a15cd
Kaito <kaitomuzicvn@gmail.com>
2026-08-29 23:38:27 +0700

    fix 13

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -6,6 +6,8 @@ const BASE_URL =
     ? 'http://localhost:5001/api'
     : 'https://todo-app-1112.onrender.com/api');
 
+console.log("Current API Base URL:", BASE_URL); // Log ra để kiểm tra xem Frontend đang bắn API đi đâu
+
 const api = axios.create({
   baseURL: BASE_URL,
   timeout: 30000,
